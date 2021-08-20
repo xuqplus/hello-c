bc63b6fb33afd00c4f8d7b5fa95d791ad941360c
xujing <xu.jing1@oyohotels.cn>
2021-08-20 22:26:34 +0800

    Begin write in cpp

---
+++ day07/user.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class User
+{
+private:
+    /* data */
+public:
+    User(/* args */);
+};
+
+User::User(/* args */)
+{
+    cout << "User::User .." << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "day07/user.cpp"
+
+using namespace std;
+
+int main()
+{
+    printf("hello 世界 ..\n");
+
+    User user;
+
+    cout << "hello 123" << endl;
+    printf("hello abc ..\n");
+    return 0;
+}
