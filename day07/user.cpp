05d0735e3ea9e87d6b608c4d4a89bb3a370d16ce
xuqplus <xuqunqun@live.cn>
2021-08-21 21:26:52 +0800

    Test copy constructor and value parameter

--- day07/user.cpp
+++ day07/user.cpp
@@ -12,7 +12,9 @@ public:
     string name;
     User(/* args */);
     User(int id, string name);
+    User(const User &obj);
     int getId();
+    void valueParam(User user);
 };
 
 User::User(/* args */)
@@ -27,7 +29,22 @@ User::User(int id, string name)
     cout << "User::User id=" << id << ", name=" << name << endl;
 }
 
+User::User(const User &obj)
+{
+    User::id = obj.id;
+    User::name = obj.name;
+    cout << "&obj=" << &obj << ", id=" << obj.id << ", name=" << obj.name << endl;
+}
+
 int User::getId(void)
 {
     return id;
+}
+
+void User::valueParam(User user)
+{
+    cout << "valueParam &user=" << &user << ", id=" << user.id << ", name=" << user.name << endl;
+    user.id = -1;
+    user.name = "modified";
+    cout << "valueParam &user=" << &user << ", id=" << user.id << ", name=" << user.name << endl;
 }
--- main.cpp
+++ main.cpp
@@ -11,8 +11,18 @@ int main()
 
     // User user;
     User user;
-    string name = "qqx";
-    User user2(1, name);
-    cout << "user2.id=" << user2.getId() << ", user2.name=" << user2.name << endl;
+    const string name = "qqx";
+    User user2(7, name);
+    // cout << "user2.id=" << user2.getId() << ", user2.name=" << user2.name << endl;
+
+    // User user3(user2);
+    // cout << "user3.id=" << user3.getId() << ", user3.name=" << user3.name << endl;
+
+    // the copy constructor will get called when parse a object as value
+    // - https://www.geeksforgeeks.org/g-fact-13/
+    cout << "afore &user2=" << &user2 << " user2.id=" << user2.getId() << ", user2.name=" << user2.name << endl;
+    user.valueParam(user2);
+    user.valueParam(user2);
+    cout << "after &user2=" << &user2 << " user2.id=" << user2.getId() << ", user2.name=" << user2.name << endl;
     return 0;
 }
