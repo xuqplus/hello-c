82847657cd29efd25457ecbff460318eeaf7579b
xujing <xu.jing1@oyohotels.cn>
2021-08-21 14:14:22 +0800

    Test c++ constructor

--- day07/user.cpp
+++ day07/user.cpp
@@ -6,11 +6,28 @@ class User
 {
 private:
     /* data */
+    int id;
+
 public:
+    int name;
     User(/* args */);
+    User(int id, int name);
+    int getId();
 };
 
 User::User(/* args */)
 {
     cout << "User::User .." << endl;
+}
+
+User::User(int id, int name)
+{
+    User::id = id;
+    User::name = name;
+    cout << "User::User id=" << id << ", name=" << name << endl;
+}
+
+int User::getId(void)
+{
+    return id;
 }
--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 int main()
 {
-    printf("hello 世界 ..\n");
+    printf("hello ..");
+    cout << "123" << endl;
 
+    // User user;
     User user;
-
-    cout << "hello 123" << endl;
-    printf("hello abc ..\n");
+    User user2(1, 1);
+    cout << "user2.id=" << user2.getId() << ", user2.name=" << user2.name << endl;
     return 0;
 }
